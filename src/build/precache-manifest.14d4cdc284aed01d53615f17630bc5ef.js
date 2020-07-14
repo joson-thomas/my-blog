091e36deaf3215fb7777badcc77f5c818d32769c
Joson Thomas <josonthomas@gmail.com>
2020-07-14 18:08:18 -0400

    Initial Commit My Blog Backend/Ux

---
+++ src/build/precache-manifest.14d4cdc284aed01d53615f17630bc5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab01dfc95a979a956be9d4bd5d1df7c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0638f2f17ea3d89383f4",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "61b81a605b7c806e89e4",
+    "url": "/static/js/2.d4df99f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4df99f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0638f2f17ea3d89383f4",
+    "url": "/static/js/main.a2941f2f.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
